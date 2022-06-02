e63fb04f466fc7b5135f4aae8132f40bb2c6fad9
David McQuiggin <david@pro-coded.com>
2022-06-03 00:05:08 +0100

    Added Serilog

--- ProRecipesWebAPI.Api/Program.cs
+++ ProRecipesWebAPI.Api/Program.cs
@@ -1,14 +1,25 @@
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+Log.Information("Starting up");
+
 var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .WriteTo.Console()
+        .ReadFrom.Configuration(ctx.Configuration)
+        .Enrich.FromLogContext());
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.UseSerilogRequestLogging();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
